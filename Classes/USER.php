f2cbe803700f2a32ee2b3f009feaaf6726c1e4a0
landryseye <landryseye99@gmail.com>
2021-03-19 22:20:06 +0000

    dsd

--- Classes/USER.php
+++ Classes/USER.php
@@ -57,4 +57,50 @@ class USER extends BDD
         );
         return $json;
     }
+    public function connecter($login, $mot_de_passe)
+    {
+        $a = $this->bdd->prepare('SELECT u_id AS user_id, u_pseudo AS pseudo, u_email AS email, u_mot_de_passe AS mot_de_passe, s_code AS statut FROM pg_utilisateurs WHERE u_pseudo = ? OR u_email = ?');
+        $a->execute(array($login, $login));
+        $utilisateur = $a->fetch();
+        if ($utilisateur) {
+            if (password_verify($mot_de_passe, $utilisateur['mot_de_passe'])) {
+
+                if ($utilisateur['statut'] == 1) {
+                    $commentaire = 'Connexion effectuée avec succès.';
+                    $json = array(
+                        'success' => true,
+                        'date' => date('d/m/Y', time()),
+                        'heure' => date('H:i:s', time()),
+                        'user_id' => $utilisateur['user_id'],
+                        'commentaire' => $commentaire
+                    );
+                } else {
+                    $commentaire = 'Votre compte est inactif. Veuillez contacter votre administrateur.';
+                    $json = array(
+                        'success' => false,
+                        'date' => date('d/m/Y', time()),
+                        'heure' => date('H:i:s', time()),
+                        'commentaire' => $commentaire
+                    );
+                }
+            } else {
+                $commentaire = 'Ce code utilisateur / email et/ou le mot de passe est incorrect.';
+                $json = array(
+                    'success' => false,
+                    'date' => date('d/m/Y', time()),
+                    'heure' => date('H:i:s', time()),
+                    'commentaire' => $commentaire
+                );
+            }
+        } else {
+            $commentaire = 'Ce code utilisateur / email est incorrect.';
+            $json = array(
+                'success' => false,
+                'date' => date('d/m/Y', time()),
+                'heure' => date('H:i:s', time()),
+                'commentaire' => $commentaire
+            );
+        }
+        return $json;
+    }
 }
--- Submit/User/connexion.php
+++ Submit/User/connexion.php
@@ -1,1 +1,38 @@
 <?php
+$parametres = array(
+    'login' => htmlentities(trim($_POST['login'])),
+    'mot_de_passe' => htmlentities(trim($_POST['mot_de_passe']))
+);
+require_once '../../Classes/CHANNEL.php';
+require_once '../../Classes/USER.php';
+$UTILISATEURS = new UTILISATEURS();
+if (!isset($_SESSION['user_id'])) {
+    $connexion = $UTILISATEURS->connecter($parametres['login'], $parametres['mot_de_passe']);
+    if($connexion['success'] == true) {
+        $_SESSION['user_id'] = $connexion['user_id'];
+        $log = $UTILISATEURS->enregistrer_log(CLIENT_ADRESSE_IP,ACTIVE_URL,'CONNEXION',json_encode($parametres),$connexion['user_id']);
+        $json = array(
+            'success' => true,
+            'date' => date('d/m/Y',time()),
+            'heure' => date('H:i:s',time()),
+            'commentaire' => $connexion['commentaire']
+        );
+    }else {
+        $json = array(
+            'success' => false,
+            'date' => date('d/m/Y',time()),
+            'heure' => date('H:i:s',time()),
+            'commentaire' => $connexion['commentaire']
+        );
+    }
+}else {
+    $commentaire = 'Une session est déjà active pour cette application. Veuillez contacter votre administrateur';
+    $json = array(
+        'success' => false,
+        'date' => date('d/m/Y',time()),
+        'heure' => date('H:i:s',time()),
+        'commentaire' => $commentaire
+    );
+}
+echo json_encode($json);
+?>
